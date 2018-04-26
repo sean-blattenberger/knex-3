036989502d55c9a99006368bbaf1c9c0e0f3628c
sean-blattenberger <sean.blattenberger@gmail.com>
2018-04-26 14:25:07 -0600

    Added env variables

--- knexfile.js
+++ knexfile.js
@@ -5,5 +5,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/drill-3-db'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + '?ssl=true'
   }
 };
